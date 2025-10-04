6dd0652aa4e99b735d7da671f02536b145747270
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-10-04 08:02:47 -0300

    feat: create project desafiobanco

---
+++ desafiobanco/src/test/java/com/desafiiobanco/desafiobanco/DesafiobancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiiobanco.desafiobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
